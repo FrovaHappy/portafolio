a2f18de8e77b483c3c4207b00d2ec649e95c2591
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-06-26 16:47:05 -0300

    💄agregado variables
    -correciones a los estilos de css y esquema de colores creado

--- src/components/docs/index.tsx
+++ src/components/docs/index.tsx
@@ -1,3 +1,4 @@
+import './index.scss'
 export type Secctions = { title: string; body: JSX.Element }[]
 
 interface Props {
@@ -10,21 +11,21 @@ function index({ sections, links }: Props) {
   return (
     <div className="docs">
       <div className="docs__contents">
-        {sections.map((section) => {
+        {sections.map((section, i) => {
           return (
-            <a href={`#${parse(section.title)}`} key={section.title}>
+            <a className="docs__contents--link" href={`#${parse(section.title)}`} key={i}>
               {section.title}
             </a>
           )
         })}
-        {sections.map((section) => {
+        {sections.map((section, i) => {
           return (
-            <>
-              <h4 id={parse(section.title)} className="docs__sections--title">
+            <div className="sectionsDocs" key={i}>
+              <h4 id={parse(section.title)} className="sectionsDocs__title">
                 {section.title + '.'}
               </h4>
-              <div className="docs__sections">{section.body}</div>
-            </>
+              <div className="sectionsDocs__body">{section.body}</div>
+            </div>
           )
         })}
 
